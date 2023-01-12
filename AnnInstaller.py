24cde741aaf61b5390400ca9cda2d5807ca72334
SLDDL <simon.masse200@gmail.com>
2023-01-12 16:59:23 +0100

    Add files via upload

--- AnnInstaller.py
+++ AnnInstaller.py
@@ -1,11 +1,15 @@
+import subprocess
 import importlib
 import os
 import tempfile
+import urllib.request
+import zipfile
 
 # Fonction qui vérifie si pip est installé et qui l'installe si ce n'est pas le cas
 def check_and_install_pip():
     try:
         import pip
+        print('Pip est déjà installé')
     except ImportError:
         print('pip n\'est pas installé, installation en cours...')
         
@@ -14,10 +18,10 @@ def check_and_install_pip():
         os.close(fd)
         
         # Télécharger le script d'installation de pip avec curl
-        !curl https://bootstrap.pypa.io/get-pip.py -o {path}
+        subprocess.run(['curl', 'https://bootstrap.pypa.io/get-pip.py', '-o', path])
         
         # Exécuter le script d'installation de pip avec python
-        !python {path}
+        subprocess.run(['python', path])
         
         # Supprimer le fichier temporaire
         os.remove(path)
@@ -29,7 +33,7 @@ def check_and_install_dependency(dependency):
         print(f'{dependency} est déjà installé')
     except ImportError:
         print(f'{dependency} n\'est pas installé, installation en cours...')
-        !pip install {dependency}
+        subprocess.run(['pip', 'install', dependency])
 
 # Vérifier et installer pip
 check_and_install_pip()
@@ -40,3 +44,25 @@ dependencies = ['tkinter', 'csv', 'ttkwidgets', 'ttkthemes']
 # Vérifier et installer chaque dépendance
 for dependency in dependencies:
     check_and_install_dependency(dependency)
+
+url = "https://github.com/rdbende/Azure-ttk-theme/archive/refs/tags/v2.1.0.zip"
+file_name = "v2.1.0.zip"
+
+# Download the zip file
+urllib.request.urlretrieve(url, file_name)
+
+# Extract the contents of the zip file to the same directory as the script
+script_dir = os.path.dirname(os.path.realpath(__file__))
+with zipfile.ZipFile(file_name, 'r') as zip_ref:
+    zip_ref.extractall(script_dir)
+
+# Delete the zip file after it has been extracted
+os.remove(file_name)
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(current_directory, 'annuaire.csv')
+
+with open(file_path, 'w') as file:
+    pass
+
+urllib.request.urlretrieve("https://github.com/SLDDL/Schoolprojets/blob/main/Annusaire3000.py",os.path.join(current_directory, 'Annusaire3000.py'))
